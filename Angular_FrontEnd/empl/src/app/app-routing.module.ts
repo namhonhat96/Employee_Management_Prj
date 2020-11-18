f982419895990c3de80335e25843920ec85dc067
Nam Ho <namhonhat96@gmail.com>
2020-11-18 18:40:19 -0500

    before reverting

--- Angular_FrontEnd/empl/src/app/app-routing.module.ts
+++ Angular_FrontEnd/empl/src/app/app-routing.module.ts
@@ -1,18 +1,14 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-<<<<<<< HEAD
+
 import { TestListComponent } from './test-list/test-list.component';
 const routes: Routes = [
   { path: 'test-list', component: TestListComponent },
   // { path: 'test-add', component: TestUpdateComponent },
 ];
-=======
-
-const routes: Routes = [];
->>>>>>> parent of 31db27d... testing
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- Angular_FrontEnd/empl/src/app/app.module.ts
+++ Angular_FrontEnd/empl/src/app/app.module.ts
@@ -3,23 +3,11 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-<<<<<<< HEAD
 import { TestListComponent } from './test-list/test-list.component';
 import { HttpClientModule } from '@angular/common/http';
 @NgModule({
   declarations: [AppComponent, TestListComponent],
   imports: [BrowserModule, AppRoutingModule, HttpClientModule],
-=======
-
-@NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
->>>>>>> parent of 31db27d... testing
   providers: [],
   bootstrap: [AppComponent],
 })
