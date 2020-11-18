c6bcfdfb667f81136f318f213802d65d29f1b69d
Nam Ho <namhonhat96@gmail.com>
2020-11-17 22:51:39 -0500

    add list-test
    add list-test

--- Angular_FrontEnd/empl/src/app/app-routing.module.ts
+++ Angular_FrontEnd/empl/src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { TestComponent } from './test/test.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'test',
+    component: TestComponent,
+  },
+  {
+    path: 'list-test',
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
