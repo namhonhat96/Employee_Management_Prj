45ccc5cc33faba4335d2f2f48792bcd936e1981b
Nam Ho <namhonhat96@gmail.com>
2020-11-19 14:04:57 -0500

    remove old spring

--- Spring_BackEnd/src/main/java/com/namho/book_projc/constant/Constant.java
+++
@@ -1,6 +0,0 @@
-package com.namho.book_projc.constant;
-
-public interface Constant {
-    String JWT_TOKEN_COOKIE_NAME = "JWT-TOKEN";
-    String SIGNING_KEY = "signingKey";
-}
--- Spring_BackEnd/src/main/java/com/namho/book_projc/service/OnBoardApp_EmployeeService.java
+++
@@ -1,6 +0,0 @@
-package com.namho.book_projc.service;
-
-public interface OnBoardApp_EmployeeService {
-    void addEmployee(int ID, int personID, String car, int visaID, String visaStartDate,
-                     String visaEndDate, String driverLisence, String driver_exp);
-}
