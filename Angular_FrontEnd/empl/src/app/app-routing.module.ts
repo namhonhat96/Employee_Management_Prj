f3352b3832b6f0ef0d1752b1916c976006396e48
kayaki13 <34051744+kayaki13@users.noreply.github.com>
2020-11-21 01:25:31 -0500

    updates on routing

--- Angular_FrontEnd/empl/src/app/app-routing.module.ts
+++ Angular_FrontEnd/empl/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { RouterModule, Routes } from '@angular/router';
+import { HousingsComponent } from './housing/housing.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'housing', component: HousingsComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
