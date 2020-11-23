843a1a763b27a544a0c66dc7ed2e878adb417f73
Duc Cuong Lai <duccuong.lai94@gmail.com>
2020-11-23 07:52:30 -0800

    dummy visa done

--- Angular_FrontEnd/empl/src/app/home-page/visa/visa.component.ts
+++ Angular_FrontEnd/empl/src/app/home-page/visa/visa.component.ts
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VisaComponent implements OnInit {
 
+  isCase1 = true;
+  isCase2 = true;
+  isCase3 = true;
+  isCase4 = true;
+  isCase5 = true;
+  isCase6 = true;
   constructor() { }
 
   ngOnInit(): void {
   }
 
+  getCase1(): boolean{
+    return this.isCase1;
+  }
+
+  getCase2(): boolean{
+    return this.isCase2;
+  }
+
+  getCase3(): boolean{
+    return this.isCase3;
+  }
+
+  getCase4(): boolean{
+    return this.isCase4;
+  }
+
+  getCase5(): boolean{
+    return this.isCase5;
+  }
+
+  getCase6(): boolean{
+    return this.isCase6;
+  }
+
+  
 }
